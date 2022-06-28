e0cbb1893bacd52c320757db38e43fc415196219
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-06-27 23:10:51 -0700

    fix: pycodestyle error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,5 +57,5 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except exception:
             pass
